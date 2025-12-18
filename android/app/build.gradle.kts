b49b8f11979332386acecea9758032e50332d066
ABDELILAH ELBAKKALI <prodmeat@Mac-mini-prodmeat.local>
2025-12-18 14:26:55 +0100

    fix: NDK 27.0.12077973 + build debug + désactiver iOS CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.mukhliss.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
     
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
